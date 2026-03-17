ab6d5b9a14229955ad946141d16bc86ba7bff60b
Ivan <ivanqqq@ukr.net>
2026-03-17 22:18:21 +0100

    sokution

--- task.sh
+++ task.sh
@@ -1,1 +1,7 @@
-#! /bin/bash
+#!/bin/bash
+
+mysqldump -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} ShopDB --routines --events > full_backup.sql
+mysqldump -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} --no-create-db --no-create-info ShopDB > data_backup.sql
+
+mysql -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} ShopDBReserve < full_backup.sql
+mysql -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} ShopDBDevelopment < data_backup.sql
