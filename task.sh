55389710ef61a921f66f4c72836473a33e6dac83
Ivan <ivanqqq@ukr.net>
2026-03-17 22:21:07 +0100

    fix

--- task.sh
+++ task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mysqldump -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} ShopDB --routines --events > full_backup.sql
-mysqldump -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} --no-create-db --no-create-info ShopDB > data_backup.sql
+mysqldump -u ${$DB_USER} -p${2:-$DB_PASSWORD} ShopDB --routines --events > full_backup.sql
+mysqldump -u ${$DB_USER} -p${2:-$DB_PASSWORD} --no-create-db --no-create-info ShopDB > data_backup.sql
 
-mysql -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} ShopDBReserve < full_backup.sql
-mysql -u ${1:-$DB_USER} -p${2:-$DB_PASSWORD} ShopDBDevelopment < data_backup.sql
+mysql -u ${$DB_USER} -p${$DB_PASSWORD} ShopDBReserve < full_backup.sql
+mysql -u ${$DB_USER} -p${$DB_PASSWORD} ShopDBDevelopment < data_backup.sql
